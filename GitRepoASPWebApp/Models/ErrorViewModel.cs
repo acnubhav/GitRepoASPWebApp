de85ab9a4321a112c396d1e37aeeeb31dc6cc23b
AnubhavSingh <acnubhav@gmail.com>
2021-12-31 15:15:39 +0530

    Add project files.

---
+++ GitRepoASPWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GitRepoASPWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
